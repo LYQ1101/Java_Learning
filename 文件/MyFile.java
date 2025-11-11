936935f75a8d90cce839e04c465b14e2e8a40892
程翊雪 <1355784761@qq.com>
2025-11-11 21:08:57 +0800

    创建文件

---
+++ 文件/MyFile.java
@@ -0,0 +1,14 @@
+import java.io.File;
+import java.io.IOException;
+
+public class MyFile {
+    public static void main(String[] args) throws IOException {
+        File file=new File("mhyfile\\aaa");
+
+        file.mkdirs();
+
+        File src=new File(file,"1.txt");
+
+        src.createNewFile();
+    }
+}
