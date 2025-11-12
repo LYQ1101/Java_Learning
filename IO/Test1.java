a0c102b8625d7c0b8d884b6ef429eedcf03dd88c
程翊雪 <1355784761@qq.com>
2025-11-12 18:56:04 +0800

    输入读取流

--- IO/Test1.java
+++ IO/Test1.java
@@ -1,3 +1,4 @@
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -8,6 +9,12 @@ public class Test1 {
         fo.write(82);
         byte [] m={77,78,79,80};
         fo.write(m,1,2);
+
+        FileInputStream input=new FileInputStream("test01");
+        int b;
+        while((b=input.read())!=-1){
+            System.out.print((char)b+"\n");
+        }
         fo.close();
     }
 }
